54a6bae09fa6530ecdfaed53c5aa3545aa48bb2a
Daniel Engel <daniel.ngl.mail@gmail.com>
2026-04-10 17:43:35 +0200

    Add more primitives

--- include/calcium/lisp_prelude.h
+++ include/calcium/lisp_prelude.h
@@ -1,10 +1,37 @@
 #ifndef LISP_PRELUDE_H
 #define LISP_PRELUDE_H
 
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/*****************************************************************************/
+/*                                 INTERFACE                                 */
+/*****************************************************************************/
+
+typedef uintptr_t LISPvalue;
+
 /*****************************************************************************/
 /*                                 PRIMITIVES                                */
 /*****************************************************************************/
 
-typedef bool LISPbool8;
+typedef bool    LISPbool8;
+typedef char    LISPchar8;
+typedef size_t  LISPusize;
+typedef uint8_t LISPbyte;
+
+typedef int8_t   LISPint8;
+typedef int16_t  LISPint16;
+typedef int32_t  LISPint32;
+typedef int64_t  LISPint64;
+typedef uint8_t  LISPuint8;
+typedef uint16_t LISPuint16;
+typedef uint32_t LISPuint32;
+typedef uint64_t LISPuint64;
+typedef float    LISPfloat32;
+typedef double   LISPfloat64;
+
+static_assert(sizeof(LISPfloat32) == 4);
+static_assert(sizeof(LISPfloat64) == 8);
 
 #endif /* LISP_PRELUDE_H */
